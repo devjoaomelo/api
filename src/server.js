524249b4951b8c14b4ac1a1f81cbd75e5c6a2aa6
joão <devjoaomelo@gmail.com>
2022-10-11 16:50:33 -0300

    post method

--- src/server.js
+++ src/server.js
@@ -2,21 +2,11 @@ const { response } = require("express");
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-    const { id, user } = request.params;
-    
-    response.send(`
-    ID da mensagem ${id}.
-    Para o usuário: ${user}.
-    `);
-});
-
-app.get("/users", (request, response) => {
-    const { page, limit } = request.query;
-    response.send(`
-    Página: ${page}. 
-    Mostrar: ${limit}`);
+app.post("/users", (request, response) => {
+    const { name, email, password } = request.body;
+    response.send(`Usuário ${name} - E-mail: ${email} e a senha ${password}`);
 })
 
 const PORT = 3333;
