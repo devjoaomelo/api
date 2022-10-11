5dd421621a53b89191f506ceb20bc42c55488736
joão <devjoaomelo@gmail.com>
2022-10-11 16:14:19 -0300

    query params

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 
 const app = express();
@@ -10,6 +11,13 @@ app.get("/message/:id/:user", (request, response) => {
     Para o usuário: ${user}.
     `);
 });
+
+app.get("/users", (request, response) => {
+    const { page, limit } = request.query;
+    response.send(`
+    Página: ${page}. 
+    Mostrar: ${limit}`);
+})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
