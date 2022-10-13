c3825338750c0e19c2201d872a62301b3dece5de
joão <devjoaomelo@gmail.com>
2022-10-13 14:51:53 -0300

    db created

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+const migrationsRun = require("./database/sqlite/migrations")
 
 const AppError = require("./utils/AppError");
 
@@ -6,10 +7,13 @@ const express = require("express");
 
 const routes = require("./routes")
 
+migrationsRun();
+
 const app = express();
 app.use(express.json());
 
 app.use(routes);
+
 
 app.use(( error, request, response, next) => {
     if(error instanceof AppError) {
